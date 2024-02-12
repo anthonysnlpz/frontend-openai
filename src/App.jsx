adf37afd800ffb7ef8c4620149d2c802ef762c8e
anthony solano lopez <anthonysnlpz@gmail.com>
2024-02-12 01:34:00 -0600

    consumo de API final

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,30 @@
 import { useState, useEffect } from 'react';
+import { generateImage } from './OpenAIAPI';
 
 function App() {
   const [prompt, setPrompt] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [buttonDisabled, setButtonDisabled] = useState(true);
-  const [remainingRequests, setRemainingRequests] = useState(null);
-  const [resetTimeRequests, setResetTimeRequests] = useState(null);
-  const [exceededLimit, setExceededLimit] = useState(false);
-  const [loading, setLoading] = useState(false); // Estado de carga
-  const [lastPrompt, setLastPrompt] = useState(null); // Guardar el último prompt utilizado
+  const [loading, setLoading] = useState(false);
+  const [lastPrompt, setLastPrompt] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true); // Activar estado de carga
-    setImageUrl(''); // Limpiar la URL de la imagen anterior
-    setButtonDisabled(true); // Deshabilitar el botón para evitar solicitudes duplicadas
-    try {
-      // URL Producción
-      // https://backend-openai-beige.vercel.app/openai
-      // URL testing
-      // http://localhost:3001/openai
-      const response = await fetch('https://frolicking-croquembouche-a22662.netlify.app/openai', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ prompt })
-      });
+    setLoading(true);
+    setImageUrl('');
+    setButtonDisabled(true);
 
-      if (!response.ok) {
-        throw new Error('Error al enviar la solicitud al servidor');
-      }
+    const imageUrl = await generateImage(prompt);
+    setImageUrl(imageUrl); // Asignar directamente la URL de la imagen
+    setLoading(false);
+    setButtonDisabled(false);
+    setLastPrompt(prompt);
 
-      const data = await response.json();
-      setImageUrl(data.url);
-
-      // Actualizar los encabezados de la tasa de solicitud
-      const remainingRequests = response.headers.get('x-ratelimit-remaining-requests');
-      const resetTimeRequests = response.headers.get('x-ratelimit-reset-requests');
-      setRemainingRequests(remainingRequests);
-      setResetTimeRequests(resetTimeRequests);
-
-      // Verificar si se ha superado el límite de solicitudes
-      if (parseInt(remainingRequests) === 0) {
-        setExceededLimit(true);
-      }
-    } catch (error) {
-      console.error('Error al enviar solicitud al servidor:', error);
-    } finally {
-      setLoading(false); // Desactivar estado de carga después de que la solicitud se complete
-      setButtonDisabled(false); // Habilitar el botón después de que la solicitud se complete
-      setLastPrompt(prompt); // Guardar el último prompt utilizado
-    }
   };
 
-  // Desactivar el botón si el prompt tiene menos de 3 caracteres, si hay una solicitud en curso o si es el mismo prompt que la solicitud anterior
   useEffect(() => {
     setButtonDisabled(prompt.length < 3 || loading || prompt === lastPrompt);
   }, [prompt, loading, lastPrompt]);
-
-  // Mostrar alerta si se ha superado el límite de solicitudes
-  useEffect(() => {
-    if (exceededLimit) {
-      alert('Has alcanzado el límite de imágenes generadas. Debes esperar antes de generar más.');
-    }
-  }, [exceededLimit]);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -79,7 +39,7 @@ function App() {
         />
         <button type="submit" disabled={buttonDisabled}>Generar imagen</button>
       </form>
-      {loading && <p>Cargando...</p>} {/* Indicador de carga */}
+      {loading && <p>Cargando...</p>}
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {imageUrl && (
           <div style={{ marginBottom: '20px' }}>
@@ -111,12 +71,6 @@ function App() {
             >
               Descargar imagen
             </button>
-          </div>
-        )}
-        {remainingRequests !== null && resetTimeRequests !== null && (
-          <div style={{ marginBottom: '20px' }}>
-            <p style={{ marginBottom: '5px' }}>Quedan <span style={{ fontWeight: 'bold' }}>{remainingRequests}</span> solicitudes.</p>
-            <p>Se restablecerán en <span style={{ fontWeight: 'bold' }}>{resetTimeRequests}</span>.</p>
           </div>
         )}
       </div>
---
+++ src/OpenAIAPI.js
@@ -0,0 +1,26 @@
+const generateImage = async (prompt) => {
+  try {
+    const response = await fetch('https://api.openai.com/v1/images/generations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer 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`
+      },
+      body: JSON.stringify({
+        prompt: prompt
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error al enviar la solicitud al servidor: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.data[0].url; // Suponiendo que la URL de la imagen está en un objeto `data` devuelto por la API
+  } catch (error) {
+    console.error('Error al enviar la solicitud al servidor:', error);
+    throw new Error('Error al enviar la solicitud al servidor:', error);
+  }
+};
+
+export { generateImage };
